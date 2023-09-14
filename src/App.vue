d835c7daea023b0759f58ff350c2af7a0ae8adf3
Paramdeo Singh <paramdeo@paramdeo.com>
2023-09-14 11:21:14 -0400

    Updates

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { ref } from 'vue'
 
-import SEO from '@components/SEO.vue'
-
 function id() {
   // all we really need is the first 8 chars for a unique ID, not the entire UUID
   return crypto.randomUUID().slice(0, 8)
@@ -66,57 +64,54 @@ const title = 'To-Do',
 
 <template>
 
-  <SEO 
-  title="To-Do"
-  description="A simple To-Do app."
-  url="https://todo.paramdeo.com"
-  og="/og.png"/>
+<div class="container">
 
   <div class="row mt-5">
+
+    <div class="col-3 d-none d-lg-block"></div>
+
     <div class="col">
+
       <h1 class="mb-3">{{ title }}</h1>
-      <div class="card mb-5">
+
+      <div class="card mb-3">
         <ul class="list-group list-group-flush">
           <li class="list-group-item fs-4 text-muted disabled" v-if="!todoList.length">No items yet.</li>
           <li class="list-group-item fs-4" v-for="todo in todoList" :key="todo.id">{{ todo.title }} <span v-on:click="deleteTodo(todo.id)" v-html="deleteIcon" style="cursor: pointer"></span></li>
         </ul>
         <div v-if="lastUpdated" class="card-footer text-end">Last updated {{ lastUpdated || '' }}</div>
       </div>
-      <div class="bg-light py-1 mb-3"></div>
+
+      <div class="bg-light mb-3" style="height: 3px"></div>
+
+      <div class="mb-3">
+        <form v-on:submit.prevent="addTodo">
+          <div class="input-group input-group-lg my-3">
+            <input type="text" class="form-control form-control-lg" placeholder="Add todo..." v-model.trim="newTodo"
+              v-bind:maxlength="charLimit" autofocus>
+            <span class="input-group-text">{{ newTodo.length }}/{{ charLimit }}</span>
+          </div>
+    
+          <button type="submit" class="btn btn-primary w-100 fs-5" v-bind:disabled="!newTodo">Add Todo</button>
+        </form>
+      </div>
+
+      <div class="d-flex justify-content-evenly">
+        <button class="btn btn-danger w-100 me-2 fs-5" v-on:click="clearTodos" v-if="todoList.length">Clear Todos</button>
+        <button class="btn btn-info w-100 me-2 fs-5" v-on:click="saveTodos">Save Todos</button>
+        <button class="btn btn-success w-100 fs-5" v-on:click="loadTodos">Load Todos</button>
+      </div>
+
+      <div class="text-center mt-3">
+        <p>Copyright &copy; <a href="https://paramdeo.com" target="_blank" rel="noopener">Paramdeo Singh</a> <strong>&middot;</strong> <a href="https://github.com/paramdeo/todo" target="_blank" rel="noopener">Source</a></p>
+      </div>
+
     </div>
+
+    <div class="col-3 d-none d-lg-block"></div>
+
   </div>
 
-  <div class="row">
-
-    <div class="mb-3">
-
-      <form v-on:submit.prevent="addTodo">
-
-        <div class="input-group input-group-lg my-3">
-          <input type="text" class="form-control form-control-lg" placeholder="Add todo..." v-model.trim="newTodo"
-            v-bind:maxlength="charLimit" autofocus>
-          <span class="input-group-text">{{ newTodo.length }}/{{ charLimit }}</span>
-        </div>
-
-        <button type="submit" class="btn btn-primary w-100 fs-5" v-bind:disabled="!newTodo">Add Todo</button>
-      </form>
-
-    </div>
-
-    <div class="d-flex justify-content-evenly">
-
-      <button class="btn btn-danger w-100 me-2 fs-5" v-on:click="clearTodos" v-if="todoList.length">Clear Todos</button>
-      <button class="btn btn-info w-100 me-2 fs-5" v-on:click="saveTodos">Save Todos</button>
-      <button class="btn btn-success w-100 fs-5" v-on:click="loadTodos">Load Todos</button>
-
-    </div>
-
-  </div>
-
-  <div class="row pt-3">
-    <div class="col text-center">
-      <p>Copyright &copy; <a href="https://paramdeo.com" target="_blank" rel="noopener">Paramdeo Singh</a> <strong>&middot;</strong> <a href="https://github.com/paramdeo/todo" target="_blank" rel="noopener">Source</a></p>
-    </div>
-  </div>
+</div>
 
 </template>
